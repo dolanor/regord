419dbb0ccdc32374b1ab6e9550886795049e9027
Tanguy ⧓ Herrmann <dolanorgit@evereska.org>
2019-04-14 15:26:36 +0200

    Add call log display

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:phone_log/phone_log.dart';
 
 void main() => runApp(MyApp());
 
@@ -45,6 +46,9 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  String _error = "";
+  final PhoneLog phoneLog = new PhoneLog();
+  Iterable<CallRecord> _callRecords;
 
   void _incrementCounter() {
     setState(() {
@@ -68,6 +72,23 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  Future<void> fetchCallLog() async {
+    PermissionStatus perm = await phoneLog.checkPermission();
+    bool res;
+    if (perm != PermissionStatus.granted) {
+      res = await phoneLog.requestPermission();
+    }
+    if (res != true) {
+      print("permission denied");
+      return;
+    }
+    Iterable<CallRecord> entries = await phoneLog.getPhoneLogs();
+    setState(() {
+      _counter = entries.length;
+      _callRecords = entries;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -76,6 +97,56 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+    final List<Widget> children = <Widget>[
+      Text(
+        'You have pushed the button this many times:',
+      ),
+      Text(
+        '$_counter',
+        style: Theme.of(context).textTheme.display1,
+      ),
+    ];
+
+    for (CallRecord call in _callRecords ?? <CallRecord>[]) {
+      children.addAll(<Widget>[
+        new Container(
+          height: 16.0,
+        ),
+        new Row(
+          children: <Widget>[
+            new Text(call.formattedNumber ?? call.number ?? 'unknow'),
+            new Padding(
+              child: new Text(call.callType),
+              padding: const EdgeInsets.only(left: 8.0),
+            ),
+          ],
+          crossAxisAlignment: CrossAxisAlignment.center,
+        ),
+        new Row(
+          children: <Widget>[
+            new Padding(
+              child: new Text(call.dateYear.toString() +
+                  '-' +
+                  call.dateMonth.toString() +
+                  '-' +
+                  call.dateDay.toString() +
+                  '  ' +
+                  call.dateHour.toString() +
+                  ': ' +
+                  call.dateMinute.toString() +
+                  ': ' +
+                  call.dateSecond.toString()),
+              padding: const EdgeInsets.only(left: 8.0),
+            ),
+            new Padding(
+                child: new Text(call.duration.toString() + 'seconds'),
+                padding: const EdgeInsets.only(left: 8.0))
+          ],
+          crossAxisAlignment: CrossAxisAlignment.center,
+        )
+      ]);
+    }
+
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -100,33 +171,30 @@ class _MyHomePageState extends State<MyHomePage> {
           // center the children vertically; the main axis here is the vertical
           // axis because Columns are vertical (the cross axis would be
           // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: children,
         ),
       ),
       floatingActionButton: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: <Widget>[
-                FloatingActionButton(
-                    onPressed: _incrementCounter,
-                    tooltip: 'Increment',
-                    child: Icon(Icons.add),
-                  ), // This trailing comma makes auto-formatting nicer for build methods.
-                FloatingActionButton(
-                    onPressed: _decrementCounter,
-                    tooltip: 'Decrement',
-                    child: Icon(Icons.add),
-                  ), // This trailing comma makes auto-formatting nicer for build methods.
+          FloatingActionButton(
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            child: Icon(Icons.add),
+          ), // This trailing comma makes auto-formatting nicer for build methods.
+          FloatingActionButton(
+            onPressed: _decrementCounter,
+            tooltip: 'Decrement',
+            child: Icon(Icons.add),
+          ), // This trailing comma makes auto-formatting nicer for build methods.
+          FloatingActionButton(
+            onPressed: fetchCallLog,
+            tooltip: 'Get number of calls',
+            child: Icon(Icons.add_call),
+          ), // This trailing comma makes auto-formatting nicer for build methods.
         ],
-           ),
+      ),
     );
   }
 }
